19b9a99790db829d478aefa3c95d28ddd2e6b58b
Van Le <van.le@pridetek.com>
2016-06-07 09:01:11 -0700

    Starting MVVM concept

---
+++ Partial_Class_Practice/Partial_Class_Practice/PartialClassOne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class_Practice
+{
+    public partial class PartialClass
+    {
+        private string _firstName, _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
---
+++ Partial_Class_Practice/Partial_Class_Practice/PartialClassTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class_Practice
+{
+    public partial class PartialClass
+    {
+        public string GetFullName()
+        {
+            return FirstName + ", " + LastName;
+        }
+    }
+}
