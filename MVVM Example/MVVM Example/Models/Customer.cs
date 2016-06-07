8482379c031091e7f5c92ee4b8b7c842f871dc32
Van Le <van.le@pridetek.com>
2016-06-07 09:24:45 -0700

    MVVM saving

---
+++ MVVM Example/MVVM Example/Models/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Example.Models
+{
+    using System;
+    using System.ComponentModel;
+    public class Customer : INotifyPropertyChanged
+    {
+        
+
+        private string _Name;
+        /// <summary>
+        /// Initializes a new instance of the Customer class
+        /// </summary>
+        public Customer(String customerName)
+        {
+            Name = customerName;
+        }
+        /// <summary>
+        /// Gets or sets the Customers name.
+        /// </summary>
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+      
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+    }
+
+}
